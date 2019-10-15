e57fa5c9c315fd027db14f5d2f2d64dfc78d62a5
Pi-Hsun Shih <pihsun@chromium.org>
2019-10-15 12:12:22 +0800

    CHROMIUM: dma-mapping: Add function for dma_addr_t->virt address in device coherent pool
    The dma address from device coherent pool can't be translated back to
    virtual address easily from outside of kernel/dma/coherent.c, since the
    struct dma_coherent_mem and related functions are not exported.
    But as in CL:1861475, in videobuf2, we would call dma_sync_* functions
    on the dma address obtained from device coherent pool, which need to
    know the virtual address from the dma address to flush the cache.
    
    Add a function to translate dma address to virtual address if the
    address is from device coherent pool.
    
    BUG=b:141592170
    TEST=build
    
    Change-Id: I5d594f128d89168f5851a275595f8f97ac47d7a1
    Signed-off-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1861476
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -160,6 +160,8 @@ int dma_mmap_from_dev_coherent(struct device *dev, struct vm_area_struct *vma,
 			    void *cpu_addr, size_t size, int *ret);
 
 void *dma_alloc_from_global_coherent(struct device *dev, ssize_t size, dma_addr_t *dma_handle);
+void *dma_to_virt_dev_coherent(struct device *dev, dma_addr_t dma_handle);
+
 int dma_release_from_global_coherent(int order, void *vaddr);
 int dma_mmap_from_global_coherent(struct vm_area_struct *vma, void *cpu_addr,
 				  size_t size, int *ret);
@@ -168,6 +170,12 @@ int dma_mmap_from_global_coherent(struct vm_area_struct *vma, void *cpu_addr,
 #define dma_alloc_from_dev_coherent(dev, size, handle, ret) (0)
 #define dma_release_from_dev_coherent(dev, order, vaddr) (0)
 #define dma_mmap_from_dev_coherent(dev, vma, vaddr, order, ret) (0)
+
+static inline void *dma_to_virt_dev_coherent(struct device *dev,
+					     dma_addr_t dma_handle)
+{
+	return NULL;
+}
 
 static inline void *dma_alloc_from_global_coherent(struct device *dev, ssize_t size,
 						   dma_addr_t *dma_handle)
